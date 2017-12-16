03fe1febd0c38cfdf648b814c797c773b684cf8f
Insane <sigurd.media@gmail.com>
2017-12-16 21:11:02 +0800

    Initial Commit

---
+++ Main.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace StringReversal
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.grpOutput = new System.Windows.Forms.GroupBox();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.lbTotalFix = new System.Windows.Forms.Label();
+            this.grpInput = new System.Windows.Forms.GroupBox();
+            this.btnRev = new System.Windows.Forms.Button();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lbTotal2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpOutput.SuspendLayout();
+            this.grpInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(6, 49);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(510, 151);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // grpOutput
+            // 
+            this.grpOutput.Controls.Add(this.lbTotal2);
+            this.grpOutput.Controls.Add(this.label2);
+            this.grpOutput.Controls.Add(this.lbTotal);
+            this.grpOutput.Controls.Add(this.lbTotalFix);
+            this.grpOutput.Controls.Add(this.txtOutput);
+            this.grpOutput.Location = new System.Drawing.Point(18, 186);
+            this.grpOutput.Name = "grpOutput";
+            this.grpOutput.Size = new System.Drawing.Size(522, 209);
+            this.grpOutput.TabIndex = 4;
+            this.grpOutput.TabStop = false;
+            this.grpOutput.Text = "Output";
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.ForeColor = System.Drawing.Color.Red;
+            this.lbTotal.Location = new System.Drawing.Point(137, 26);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(0, 13);
+            this.lbTotal.TabIndex = 5;
+            // 
+            // lbTotalFix
+            // 
+            this.lbTotalFix.AutoSize = true;
+            this.lbTotalFix.Location = new System.Drawing.Point(16, 26);
+            this.lbTotalFix.Name = "lbTotalFix";
+            this.lbTotalFix.Size = new System.Drawing.Size(116, 13);
+            this.lbTotalFix.TabIndex = 4;
+            this.lbTotalFix.Text = "Total with whitespace :";
+            // 
+            // grpInput
+            // 
+            this.grpInput.Controls.Add(this.btnRev);
+            this.grpInput.Controls.Add(this.btnClr);
+            this.grpInput.Controls.Add(this.txtInput);
+            this.grpInput.Location = new System.Drawing.Point(12, 13);
+            this.grpInput.Name = "grpInput";
+            this.grpInput.Size = new System.Drawing.Size(522, 167);
+            this.grpInput.TabIndex = 5;
+            this.grpInput.TabStop = false;
+            this.grpInput.Text = "Input";
+            // 
+            // btnRev
+            // 
+            this.btnRev.Location = new System.Drawing.Point(181, 127);
+            this.btnRev.Name = "btnRev";
+            this.btnRev.Size = new System.Drawing.Size(75, 33);
+            this.btnRev.TabIndex = 5;
+            this.btnRev.Text = "Reverse";
+            this.btnRev.UseVisualStyleBackColor = true;
+            this.btnRev.Click += new System.EventHandler(this.btnRev_Click);
+            // 
+            // btnClr
+            // 
+            this.btnClr.Location = new System.Drawing.Point(287, 127);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(75, 33);
+            this.btnClr.TabIndex = 4;
+            this.btnClr.Text = "Clear";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.btnClr_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(6, 34);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(510, 87);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lbTotal2
+            // 
+            this.lbTotal2.AutoSize = true;
+            this.lbTotal2.ForeColor = System.Drawing.Color.Red;
+            this.lbTotal2.Location = new System.Drawing.Point(415, 26);
+            this.lbTotal2.Name = "lbTotal2";
+            this.lbTotal2.Size = new System.Drawing.Size(0, 13);
+            this.lbTotal2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Total without whitespace :";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 400);
+            this.Controls.Add(this.grpInput);
+            this.Controls.Add(this.grpOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Reversal / Count Char";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.grpOutput.ResumeLayout(false);
+            this.grpOutput.PerformLayout();
+            this.grpInput.ResumeLayout(false);
+            this.grpInput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.GroupBox grpOutput;
+        private System.Windows.Forms.Label lbTotalFix;
+        private System.Windows.Forms.GroupBox grpInput;
+        private System.Windows.Forms.Button btnRev;
+        private System.Windows.Forms.Button btnClr;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lbTotal;
+        private System.Windows.Forms.Label lbTotal2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace StringReversal
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRev_Click(object sender, EventArgs e)
+        {
+        
+            try
+            {
+                //Input dari 'txtInput.Text' window akan diletakkan di 'inptString' string
+
+                string inptString = (txtInput.Text);
+
+                // convert string dari ex : saya suka makan => ayas akus nakam ;
+
+                string resultString = string.Join(" ", inptString       // resultString di cantum sini
+                    .Split(' ')                                         // setiap string akan dijarakkan dengan satu space " "
+                    .Select(x => new String(x.Reverse().ToArray())));   // x akan di-reverse kan dgn format setiap string dan .Select
+
+                txtOutput.Text = resultString;                          // resultString akan masuk ke window 'txt.Output'                
+            }
+            catch (Exception upin)
+            {
+                MessageBox.Show(upin.Message);                           // exception catch ......
+            }
+        }
+
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";     // kosongkan input window bila klik 
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            //Jumlah perkataan dgn tempat kosong
+
+            lbTotal2.Text = txtInput.Text.Length.ToString();
+
+            // Jumlah perkataan tanpa tempat kosong
+
+            int total = txtInput.Text.Count(char.IsLetter);          
+            lbTotal.Text = total.ToString();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            txtOutput.ReadOnly = true;                                  // Ouput window x boleh edit
+            txtOutput.BackColor = System.Drawing.SystemColors.Window;   // Warna output window putih
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Kita buat satu window untuk tanya user apabila depa klik button 'X', bila klik 'YES' program akan tutup
+
+            DialogResult exitDialog = MessageBox.Show("Are you sure want to exit? ", "Exit", MessageBoxButtons.YesNo);
+            if (exitDialog == DialogResult.Yes)
+            {
+                this.Dispose();                             // Dispose digunakan untuk release memory yang digunakan dari application ni
+                Application.Exit();                         // Application tutup
+            }
+            
+            // Bila klik 'NO' window exit akan tutup
+
+            else if (exitDialog == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
